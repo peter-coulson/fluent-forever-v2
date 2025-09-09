b31e642a1ec881e19a4aa3f5bca4f371f82ef0db
peter_coulson <peterjcouls@gmail.com>
2025-09-09 15:22:08 -0500

    removed legacy provider code

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -5,16 +5,8 @@ Abstracts all external dependencies (APIs, data sources, sync targets) into
 pluggable providers that can be mocked, swapped, or configured independently.
 """
 
-from .context_helper import (
-    get_provider_from_context,
-    register_provider_in_context,
-    setup_providers_in_context,
-)
 from .registry import get_provider_registry
 
 __all__ = [
     "get_provider_registry",
-    "setup_providers_in_context",
-    "get_provider_from_context",
-    "register_provider_in_context",
 ]
